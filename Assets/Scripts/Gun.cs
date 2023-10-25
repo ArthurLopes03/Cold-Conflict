80674c2f2654c29a8afcbefac3b8bf63c30686c0
ArthurLopes03 <C22509166@mytudublin.ie>
2023-10-25 09:28:38 +0100

    Movement Script

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [Header("Controls")]
+    public KeyCode shootKey = KeyCode.Mouse0;
+    public KeyCode aimKey = KeyCode.Mouse1;
+    public KeyCode reload = KeyCode.R;
+
+}
